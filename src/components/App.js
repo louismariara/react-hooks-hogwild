9eac34a712055c0d7e8cbb767c9f34ffeae93f87
Louis Githogori Mariara <louismariara@gmail.com>
2024-11-05 23:07:13 +0300

    record 7

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import hogsData from "../porkers_data"; // data from porkers_data.js
+import hogsData from "../porkers_data"; 
 import HogList from "./HogList";
 import FilterSort from "./FilterSort";
 import HogForm from "./HogForm";
@@ -9,42 +9,42 @@ function App() {
   const [showGreased, setShowGreased] = useState(false);
   const [sortBy, setSortBy] = useState("name");
 
-  // Toggles greased filter
+  
   const handleToggleGreased = () => setShowGreased((prev) => !prev);
 
-  // Sets the sorting criteria
+  
   const handleSortChange = (criteria) => setSortBy(criteria);
 
-  // Adds a new hog to the list
+  
   const addNewHog = (newHog) => {
     setHogs((prevHogs) => [...prevHogs, newHog]);
   };
 
-  // Filters hogs by greased status
+  
   const filteredHogs = hogs.filter((hog) =>
     showGreased ? hog.greased : true
   );
 
-  // Sorts hogs by the selected criteria
+  
   const sortedHogs = [...filteredHogs].sort((a, b) =>
     a[sortBy] > b[sortBy] ? 1 : -1
   );
 
   return (
     <div className="App" style={{ position: "relative", padding: "20px" }}>
-      {/* Move the HogForm to a more prominent place */}
+    
       <div style={{ marginBottom: "20px" }}>
         <h2>Add a New Hog</h2>
         <HogForm addNewHog={addNewHog} />
       </div>
 
-      {/* Filter and Sort options */}
+      
       <FilterSort
         onToggleGreased={handleToggleGreased}
         onSortChange={handleSortChange}
       />
 
-      {/* List of hogs */}
+    
       <HogList hogs={sortedHogs} />
     </div>
   );
